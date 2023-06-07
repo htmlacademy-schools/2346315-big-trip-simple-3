d9a5464e7b7ebeeffc33f23a10a08d32b045866e
Siwirus <kotyukov.iv@edu.spbstu.ru>
2023-06-07 17:42:40 +0300

    task3

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Presenter from './presenter';
-import FiltersView from './view/FiltersView';
-import { render } from './render';
+import Presenter from './presenter.js';
+import Filters from './view/filters.js';
+import { render } from './render.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
-render(new FiltersView(), filtersContainer);
+render(new Filters(), filtersContainer);
 
 const container = document.querySelector('.trip-events');
 const tripPresenter = new Presenter({container: container});
